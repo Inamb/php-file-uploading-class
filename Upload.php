f77ccf4d55ba437976e873130e866956765ceb1b
Inam Ul Huq <inamulhuq44@gmail.com>
2017-08-24 07:15:07 +0500

    extension validation added
    added a extension validator function that will validate extension as well.

--- Upload.php
+++ Upload.php
@@ -6,55 +6,48 @@
 class Upload
 {
 	private $protocol;
+	private $uploaded = false;
+
+	private $file_extension;
 	private $field_name;
 	private $file_name;
 	private $file_tmp_name;
 	private $file_size_bytes;
+
+
 	private $dir;
 	private $errors = [];
-	private $allowed_exts = ["jpg","png","png"];
+	private $allowed_extensions = ["jpg","png","gif"];
 	private $allowed_size_bytes;
-	private $uploaded = false;
+	
 
-	function __construct( $field_name = null,$dir = null,$max_size_mb = 5,$allowed_exts = null )
+	function __construct( $field_name = null,$dir = null,$max_size_mb = 5,$allowed_extensions = null )
 	{
 		if ($field_name == null	or $field_name == "" or !isset($_FILES[$field_name])) 
 		{
 			$this->errors['field_name_error'] = "YOU CAN'T LEAVE INPUT FIELD NAME PARAMETER EMPTY";
-		
 		}else{
-
 			$this->field_name = $field_name;
-			
 			$this->file_tmp_name = $_FILES[$this->field_name]['tmp_name'];
 			$this->file_name = $_FILES[$this->field_name]['name'];
-
 			$this->file_size_bytes = $_FILES[$this->field_name]['size'];
-
+			$this->file_extension = pathinfo($this->file_name)['extension'];
 		}
 
 		if ( $dir == null or $dir == "" ) {
-			
 			$this->errors["upload_dir_error"] = "YOU CAN'T LEAVE UPLOAD DIRECTORY PARAMETER EMPTY";
-		
 		}else{
-
 			$this->dir = $dir;
-
 		}
 
 		if ($max_size_mb < 1000) {
-			
-			$this->allowed_size_bytes = $this->toByte($max_size_mb);	
-		
+			$this->allowed_size_bytes = $this->to_byte($max_size_mb);	
 		}else{
-			
 			$this->errors['max_size_error'] = "ENTERED WRONG MAX SIZE VALUE CAN'T BE MORE THEN 1000 MB";
-
 		}
-		if ($allowed_exts != null) {
-			if (is_array($allowed_exts)) {
-				$this->allowed_exts = $allowed_exts;
+		if ($allowed_extensions != null and $allowed_extensions != "") {
+			if (is_array($allowed_extensions)) {
+				$this->allowed_extensions = $allowed_extensions;
 			}else{
 				$this->errors['allowed_extensions_error'] = "THERE IS SOME PROBLEM WITH YOUR PASSED ARRAY FOR ALLOWED EXTENSIONS";
 			}
@@ -62,30 +55,39 @@ class Upload
 		
 	}
 
-	private function toByte($mb)
+	private function to_byte($mb)
 	{
 		return $mb*1048576;
 	}
-	private function toMB($bytes)
+	private function to_mb($bytes)
 	{
 		return $bytes/1048576;
 	}
 
 	private function validate_size()
 	{
-
 		if ($this->allowed_size_bytes < $this->file_size_bytes) {
 			
-			$this->errors['file_size'] = "FILE SIZE CAN'T BE MORE THEN ".$this->toMB($this->allowed_size_bytes)." MB. THIS FILE SIZE IS ".$this->toMB($this->file_size_bytes)." MB";
-
+			$this->errors['file_size'] = "FILE SIZE EXCEEDS MAX FILE SIZE LIMIT";
 			return false;
 		}
 		return true;
 	}
 
+	private function validate_extension()
+	{
+		if (!in_array($this->file_extension, $this->allowed_extensions)) {
+			$this->errors['invalid_file_extension'] = "YOUR FILE EXTENSION DOSN'T MATCH ALLOWED EXTENSIONS";
+			return false;
+		}
+		return true;
+	}
+
+
+
 	public function do_upload()
 	{
-		if (count($this->errors) == 0 and $this->validate_size()) {
+		if (count($this->errors) == 0 and $this->validate_size() and $this->validate_extension()) {
 			$this->uploaded = move_uploaded_file($this->file_tmp_name, $this->dir . $this->file_name);
 			if($this->uploaded){
 				return true;
